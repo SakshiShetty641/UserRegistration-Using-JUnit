521cce79510ef1b12c751d89cd03b1663cf04c71
Sakshi <sakshishetty1231997@gmail.com>
2021-08-29 14:51:00 +0530

    Added: Welcome message to the program

---
+++ README.md
@@ -0,0 +1,1 @@
+# UserRegistration-Using-JUnit
---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To implement J-Unit Test Cases
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-08-29
+ */
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Program");
+    }
+}
